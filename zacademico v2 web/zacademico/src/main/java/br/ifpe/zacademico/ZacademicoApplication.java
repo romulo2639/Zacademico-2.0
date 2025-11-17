8e7dc758bc6d795a037505d5436a4d417c8ec5fb
Romulo <roargs2009@gmail.com>
2025-11-17 11:29:03 -0300

    Add files via upload

--- zacademico v2 web/zacademico/src/main/java/br/ifpe/zacademico/ZacademicoApplication.java
+++ zacademico v2 web/zacademico/src/main/java/br/ifpe/zacademico/ZacademicoApplication.java
@@ -2,8 +2,10 @@ package br.ifpe.zacademico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.ifpe.zacademico.spring_boot", "br.ifpe.zacademico.model"})
 public class ZacademicoApplication {
 
 	public static void main(String[] args) {
